cb6972c339508cb366607ffeb7b699d0774baf78
Feesterra <elena.golosovskaia@gmail.com>
2018-05-11 12:10:18 +0300

    Upgraded version

--- src/download.py
+++ src/download.py
@@ -1,4 +1,5 @@
-"""Downloads pictures from URLs and """
+"""Downloads pictures from URLs and saves them to a particular directory"""
+
 import argparse
 import os
 import time
@@ -16,6 +17,7 @@ parser.add_argument('--size', help='sets size needed', default='100x100')
 args = parser.parse_args()
 
 error_counter = 0
+saved = 0
 
 
 def size_type_convert():
@@ -25,6 +27,9 @@ def size_type_convert():
     args.size = tuple(map(int, args.size))
     if len(args.size) != 2:
         raise Exception("Wrong size typing, please, use XXXxXXX format. Example: 200x200")
+
+
+size_type_convert()
 
 
 def file_read(file):
@@ -42,45 +47,51 @@ def file_read(file):
 
 
 def statistics(saved):
-    """Prints statitics for downloaded images"""
+    """Prints statistics for downloaded images"""
 
     print('Files downloaded: {}\n'.format(saved) + \
           'Errors occured: {}\n'.format(error_counter) + \
           'Total time: {:.2f}'.format(time.time() - init))
 
 
-def main_func():
-    """Does all work for downloading and changing images"""
+def main_func(url):
+    """Downloads picture from url and saves it
 
-    saved = 0
-    size_type_convert()
-    urls = file_read('/home/sh/urllist.txt')
-    for i, url in urls:
-        try:
-            img = Image.open(urllib.request.urlopen(url))
-        except Exception:
-            global error_counter
-            error_counter += 1
-            continue
+    :param url: tuple(picture number, url)
+    """
 
-        filename = "{0:05}.jpg".format(i)
-        path = os.path.join(args.dir, filename)
+    global saved
 
-        with open(path, 'w') as file:
-            img.thumbnail(args.size)
-            img.save(file, "JPEG")
-            saved += 1
-        print("Finished {}".format(path))
-    statistics(saved)
+    if not os.path.exists(args.dir):
+        os.mkdir(args.dir)
+
+    try:
+        img = Image.open(urllib.request.urlopen(url[1]))
+    except Exception:
+        global error_counter
+        error_counter += 1
+
+    filename = "{0:05}.jpg".format(url[0])
+    path = os.path.join(args.dir, filename)
+
+    with open(path, 'wb') as file:
+        img.thumbnail(args.size)
+        img.save(file, "JPEG")
+
+        saved += 1
+    print("Finished {}".format(path))
 
 
 if __name__ == '__main__':
     init = time.time()
-    if not os.path.exists(args.dir):
-        os.mkdir(args.dir)
+    urls = file_read('/home/sh/urllist.txt')
 
-    pool = ThreadPool(int(args.threads))
-    pool.apply_async(main_func)
+    x = int(args.threads)
+
+    pool = ThreadPool(x)
+    pool.map(main_func, urls)
 
     pool.close()
     pool.join()
+
+    statistics(saved)
